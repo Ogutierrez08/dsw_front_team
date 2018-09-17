a3f495dfd8c7cd74f1127f570364217547cc8651
Renato Echevarria <rmev_97@hotmail.com>
2018-09-16 21:21:19 -0500

    Todavía fraccionando proyecto

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -2,11 +2,31 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AboutUsComponent } from './../about-us/about-us.component';
+import { ProjectsComponent } from '../projects/projects.component';
+import { GraphicsComponent } from '../graphics/graphics.component';
+import { TeamComponent } from '../team/team.component';
+import { ContactUsComponent } from '../contact-us/contact-us.component';
 
 const routes: Routes = [
   {
     path: 'about-us',
     component: AboutUsComponent
+  },
+  {
+    path: 'projects',
+    component: ProjectsComponent
+  },
+  {
+    path: 'graphics',
+    component: GraphicsComponent
+  },
+  {
+    path: 'team',
+    component: TeamComponent
+  },
+  {
+    path: 'contact-us',
+    component: ContactUsComponent
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,19 @@ import { MatButtonModule, MatCheckboxModule } from '@angular/material';
 import { AppComponent } from './app.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { AppRoutingModule } from './app-routing/app-routing.module';
+import { ProjectsComponent } from './projects/projects.component';
+import { GraphicsComponent } from './graphics/graphics.component';
+import { TeamComponent } from './team/team.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AboutUsComponent
+    AboutUsComponent,
+    ProjectsComponent,
+    GraphicsComponent,
+    TeamComponent,
+    ContactUsComponent
   ],
   imports: [
     BrowserModule,
@@ -22,5 +30,4 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
   bootstrap: [AppComponent]
 })
 export class AppModule {
-  title: string = 'hmmmm';
 }
