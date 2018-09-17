2d6189ab07ecfa8837911e02d6c6f47763f66c78
Renato Echevarria <rmev_97@hotmail.com>
2018-09-16 20:28:47 -0500

    Fraccionando proyecto en componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatCheckboxModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { AppRoutingModule } from './app-routing/app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutUsComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    [MatButtonModule, MatCheckboxModule],
+    AppRoutingModule,
+    [ MatButtonModule, MatCheckboxModule ]
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  title: string = 'hmmmm';
+}
