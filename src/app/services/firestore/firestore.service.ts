7f50e990912366c18eca6d69f5ad94faadeb9ccb
Carlos Noel Bernaola <carlosnoelg3@outlook.com>
2018-11-10 20:29:05 -0500

    Create Empleado Component al 70%, no guarda en la base de datos y el estilo no es el adecuado

--- src/app/services/firestore/firestore.service.ts
+++ src/app/services/firestore/firestore.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { AngularFirestore, AngularFirestoreDocument, AngularFirestoreCollection } from 'angularfire2/firestore';
 import { Observable } from 'rxjs';
 import {AbogadosInterface} from 'src/app/models/abogados'
+import { EmpleadosInterface } from 'src/app/models/empleados';
 
 @Injectable({
   providedIn: 'root'
@@ -10,8 +11,11 @@ export class FirestoreService {
 
   abogadosColecction: AngularFirestoreCollection<AbogadosInterface>
 
+  empleadoColecction: AngularFirestoreCollection<EmpleadosInterface>
+
   constructor(private firestore: AngularFirestore) { 
   this.abogadosColecction = firestore.collection<AbogadosInterface>('abogados');
+  this.empleadoColecction = firestore.collection<EmpleadosInterface>('empleados');
   }
 
   public createAbogado(abogado:AbogadosInterface, onSuccess:Function){
@@ -33,6 +37,20 @@ export class FirestoreService {
   
   public updateAbogado(documentId: string, data: any) {
     return this.firestore.collection('abogados').doc(documentId).set(data);
+  }
+
+  /**
+   * Empleado - CRUD
+   */
+  public crearEmpleado(empleado:EmpleadosInterface, onSuccess:Function) {
+    this.empleadoColecction.add(empleado).then((value) =>{
+      console.log('value', value);
+      onSuccess();
+    })
+  }
+
+  public getEmpleados(){
+    return this.firestore.collection('empleados').snapshotChanges();
   }
 
 }
